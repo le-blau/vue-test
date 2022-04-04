aef79b474d5d449fd77938c60728b55f31cd53b9
blau <le.space@icloud.com>
2022-04-04 23:57:12 +0900

    データを配列に追加

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const app = Vue.createApp({
   data: () => ({
-    newItem: ''
+    newItem: '',
+    todos: []
   }),
   methods: {
     addItem: function(event) {
       console.log('clicked');
+      let todo = {
+        item: this.newItem
+      }
+      this.todos.push(todo) // オブジェクトを配列に追加
     }
   }
 })

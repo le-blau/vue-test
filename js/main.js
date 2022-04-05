22cd656a987d408912d42ac9d291445d5409470c
blau <le.space@icloud.com>
2022-04-06 00:18:57 +0900

    v-once

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const app = Vue.createApp({
   data: () => ({
-    
-  })
+    message: 'hello'
+  }),
+  methods: {
+    clickHandler: function(event) {
+      // 文字列を反転
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
 })
 app.mount('#app')

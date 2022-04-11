d15d1ff2f5b8099f3ef7975ab3f70da97ad893e6
blau <le.space@icloud.com>
2022-04-12 00:59:08 +0900

    computed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 const app = Vue.createApp({
   data: () => ({
-    
-  })
+    message: 'hello'
+  }),
+  computed: {
+    reversedMessage: function() {
+      return this.message.split('').reverse().join('')
+    }
+  },
+  methods: {
+    reversedMessageMethod: function() {
+      return this.message.split('').reverse().join('')
+    }
+  }
 })
 app.mount('#app')
